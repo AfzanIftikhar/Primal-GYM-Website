331d0bb49a494f5bbff19cf98d1a4c13b56f0cec
Afzan Iftikhar <aafzan95@gmail.com>
2025-11-25 11:43:32 +0500

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
-import { StrictMode } from 'react'
+ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
-
-<App />
-  </HashRouter>
-
-  
-  
-  
-)
+  // 👇 Add the basename prop here
+  <BrowserRouter basename="/Primal-GYM-Website/">
+    <App />
+  </BrowserRouter>
+);
